9728461a3f35b1160cc2819685471720fa3b771a
IsuriHarischandra <isuriharischandra2000@gmail.com>
2025-05-07 20:03:14 +0530

    Delete api.js

--- api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'http://localhost:8080/api';
-
-export const getPosts = () => axios.get(`${BASE_URL}/posts`);
-export const createPost = (post) => axios.post(`${BASE_URL}/posts`, post);
-export const deletePost = (id) => axios.delete(`${BASE_URL}/posts/${id}`);
